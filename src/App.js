0ff98dd09bf808d0b231bb1b40b91b6a09831d22
averagealloy <m.mctighe10@gmail.com>
2020-02-17 13:10:37 -0500

    app lay out done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="container">
+        <aside id="sidebar">Users</aside>
+        <section id="main">
+          <section id="messages-list">Messages List</section>
+          <section id="new-message">New Message</section>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
