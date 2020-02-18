d90718f2bbdea5ccd132bb79eafc0e23b7f9cba0
averagealloy <m.mctighe10@gmail.com>
2020-02-18 10:56:52 -0500

    add message component

---
+++ src/components/Addmessage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Addmessage = (props) => {
+    let input 
+    return(
+        <section id="new-message">
+            <input 
+                onKeyPress={(e) => {
+                    if (e.key === 'Enter') {
+                        props.dispatch(input.value, 'ME')
+                        input.value = " "
+                    }
+                }}
+                type="text"
+                ref={(node) => {
+                    input = node
+                }}
+                />
+        </section>
+    )
+}
+
+Addmessage.PropTypes = {
+    dispatch: PropTypes.func,isRequired
+}
+
+export default AddMessage
